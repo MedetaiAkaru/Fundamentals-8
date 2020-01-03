a953fa5e68f9b6e68f6d126c478634eec6c32278
Joel Lim <akaru@littlix.com>
2020-01-03 11:17:46 +0800

    updated

--- exercise_51.rb
+++ exercise_51.rb
@@ -1,13 +1,33 @@
-# In Class Exercise: Binary Search
-# Write a method, binary search(array, target), that accepts an array and another input as target
-# The method should use the binary search algorithm to return the position of the element where the target is found
+# You will write a method, binary search(array, target), that accepts an array and another input as target
+# The method should use the iterative binary search algorithm to return the position of the element where the target is found
+# If no target is found, -1 should be returned
+# The recursive solution is not accepted for this exercise
 
+# Write Your method code here
 def binary_search(array, target)
-	#Your code here
+
 end
 
-test_array = (100..200).to_a
-puts binary_search(135, test_array) == 35
+# Driver Code: Do not edit under this line
 
-test_array = [13, 19, 24, 29, 32, 37, 43]
-puts binary_search(35, test_array) == -1
+# check_solution runs a single test case and prints whether it was
+# successful or not.
+def check_solution(test_number, array, target, expected)
+  actual = binary_search(array, target)
+  if actual != expected
+    puts "Test ##{test_number}: Incorrect value: got #{actual}, expected #{expected}"
+    return false
+  end
+
+  puts "Test ##{test_number}: Correct"
+  return true
+end
+
+# run_tests runs each of the test cases.
+def run_tests
+  check_solution(1, (100..200).to_a, 135, 35)
+  check_solution(2, [13, 19, 24, 29, 32, 37, 43], 35, -1)
+end
+
+# Execute the tests.
+run_tests
--- exercise_59/fibonacci.rb
+++
@@ -1,7 +0,0 @@
-def iterative_nth_fibonacci_number(n)
-
-end
-
-def recursive_nth_fibonacci_number(n)
-    
-end
